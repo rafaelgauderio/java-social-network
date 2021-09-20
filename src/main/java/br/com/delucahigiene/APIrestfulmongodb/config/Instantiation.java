40f187ea5b8488d039cff87f58ecbeb5ecdfa270
Rafael De Luca <deluca1712@gmail.com>
2021-09-20 19:52:52 -0300

    Instaciacao da base de dados. Classe Instantiation.

---
+++ src/main/java/br/com/delucahigiene/APIrestfulmongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package br.com.delucahigiene.APIrestfulmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import br.com.delucahigiene.APIrestfulmongodb.domain.User;
+import br.com.delucahigiene.APIrestfulmongodb.repository.UserRepository;
+
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String...arg0) throws Exception {
+			
+		userRepository.deleteAll();
+		
+		User rafael = new User (null,"Rafael De Luca", "rafael@gmail");
+		User maria = new User(null, "Maria da Silva", "maria@gmail.com");
+		User alex = new User(null, "Alex Pereira", "alex@gmail.com");
+		User robert = new User(null, "Robert Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(rafael,maria,alex,robert));
+		
+		
+	}
+
+}
