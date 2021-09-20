072c861c1b9cf5176e054dbf75e3c077cb1c5cf3
Rafael De Luca <deluca1712@gmail.com>
2021-09-20 13:39:18 -0300

    Entity User, UserResource, REST funcionando

---
+++ src/main/java/br/com/delucahigiene/APIrestfulmongodb/resources/UserResource.java
@@ -0,0 +1,30 @@
+package br.com.delucahigiene.APIrestfulmongodb.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.delucahigiene.APIrestfulmongodb.domain.User;
+
+@RestController
+@RequestMapping(value="users")
+public class UserResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		
+		User rafael = new User("1", "Rafael de Luca", "rafael@gmail.com");
+		User maria = new User("2", "Maria da Silva","maria@gmail.com");
+		List<User> list = new ArrayList<User>();
+		list.addAll(Arrays.asList(rafael,maria));
+		return ResponseEntity.ok().body(list);		
+		
+	}
+	
+
+}
