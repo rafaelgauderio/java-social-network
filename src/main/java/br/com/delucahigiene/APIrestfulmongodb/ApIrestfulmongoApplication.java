6430d9ac1e2b0f1d14614f8878fa06aadb444af3
Rafael De Luca <deluca1712@gmail.com>
2021-09-20 12:17:31 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/delucahigiene/APIrestfulmongodb/ApIrestfulmongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.delucahigiene.APIrestfulmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIrestfulmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIrestfulmongoApplication.class, args);
+	}
+
+}
