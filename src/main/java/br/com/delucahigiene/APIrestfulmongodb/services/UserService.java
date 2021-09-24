4777ed35a43b27b364194cd0162283b557c2003c
Rafael De Luca <deluca1712@gmail.com>
2021-09-24 11:41:56 -0300

    Insert um usuario, método POST

--- src/main/java/br/com/delucahigiene/APIrestfulmongodb/services/UserService.java
+++ src/main/java/br/com/delucahigiene/APIrestfulmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.delucahigiene.APIrestfulmongodb.domain.User;
+import br.com.delucahigiene.APIrestfulmongodb.dto.UserDTO;
 import br.com.delucahigiene.APIrestfulmongodb.repository.UserRepository;
 
 @Service
@@ -23,10 +24,16 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado"));
-		
-		
+			
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 	
 	
 
