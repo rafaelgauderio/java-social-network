896d315db4e33a371751e13c01477dd30ebf406a
Rafael De Luca <deluca1712@gmail.com>
2021-09-29 11:20:04 -0300

    Adicionando comentarios aos posts do usuarios

--- src/main/java/br/com/delucahigiene/APIrestfulmongodb/config/Instantiation.java
+++ src/main/java/br/com/delucahigiene/APIrestfulmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.delucahigiene.APIrestfulmongodb.domain.Post;
 import br.com.delucahigiene.APIrestfulmongodb.domain.User;
 import br.com.delucahigiene.APIrestfulmongodb.dto.AuthorDTO;
+import br.com.delucahigiene.APIrestfulmongodb.dto.CommentDTO;
 import br.com.delucahigiene.APIrestfulmongodb.repository.PostRepository;
 import br.com.delucahigiene.APIrestfulmongodb.repository.UserRepository;
 
@@ -45,6 +46,16 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null,sdf.parse("21/08/2021"), "Bora ir pra praia","Vamos viajar para Miami, abraços.",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("25/03/2021"),"Bem vindo a Foz.","Hoje vai ser bom o passeio.",new AuthorDTO(maria));
 		Post post3 = new Post(null,sdf.parse("17/12/2020"),"Aniversário","Sucesso hoje faço quase 40 anos", new AuthorDTO(rafael));
+		
+		CommentDTO comt1 = new CommentDTO("Boa viajem pra praia meu jovem!", sdf.parse("22/08/2021"), new AuthorDTO(alex));
+		CommentDTO comt2 = new CommentDTO("Curta Foz do iguaçu, visita as Cataratas.",sdf.parse("25/03/2021"),new AuthorDTO(rafael));
+		CommentDTO comt3 = new CommentDTO("Aproveite para visitar o Paraguai.",sdf.parse("25/03/2021"),new AuthorDTO(robert));
+		CommentDTO comt4 = new CommentDTO("Faça um festão de aniversário e me convide",sdf.parse("17/12/2020"),new AuthorDTO(maria));
+		
+		
+		post1.getComments().addAll(Arrays.asList(comt1));
+		post2.getComments().addAll(Arrays.asList(comt2,comt3));
+		post3.getComments().addAll(Arrays.asList(comt4));
 		
 		
 		postRepository.saveAll(Arrays.asList(post1,post2,post3));
--- src/main/java/br/com/delucahigiene/APIrestfulmongodb/domain/Post.java
+++ src/main/java/br/com/delucahigiene/APIrestfulmongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package br.com.delucahigiene.APIrestfulmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 
 import br.com.delucahigiene.APIrestfulmongodb.dto.AuthorDTO;
+import br.com.delucahigiene.APIrestfulmongodb.dto.CommentDTO;
 
 public class Post implements Serializable {
 	
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -71,6 +76,15 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments=comments;
+	}
+	
 
 	@Override
 	public int hashCode() {
